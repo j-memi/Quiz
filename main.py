f579376cf10879dd19acda2ba0baae7ff820f343
j-memi <qujimmy0@live.com>
2022-05-11 15:15:24 +1200

    loading quiz file error handling

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def show_frame(frame):
 
 def get_quiz():
     try:
-        quiz_info = open("dependencies//quizzes.json", "r")
+        quiz_info = open("dependencies//quizzs.json", "r")
         quiz_info = json.load(quiz_info)
         quiz_info = quiz_info["Quizzes"]
         quiz_list = []
@@ -17,6 +17,10 @@ def get_quiz():
         return quiz_list
     except FileNotFoundError:
         print("File not found")
+        quiz_list = []
+        return quiz_list
+    except json.decoder.JSONDecodeError:
+        print("File is not in JSON format")
         quiz_list = []
         return quiz_list
 
@@ -87,55 +91,76 @@ load_button.grid(row=0, column=1, sticky="ew", padx=106, pady=50)
 buttons.grid(row=1, column=0, sticky="ew")
 
 # ---------- Code for Load Quiz Screen ----------
-# Heading text
-heading = tk.Label(
-    master=load_quiz_frame,
-    text="Choose a quiz!",
-    font=("Helvetica", 32),
-    bg="white"
-    )
-heading.grid(row=0, column=0, padx=150, pady=100)
-# Quiz Selector
-load_quiz_frame.grid_columnconfigure(0, weight=1)
-canvas_container = tk.Canvas(load_quiz_frame, height=480)
-options = tk.Frame(canvas_container)
-scrollbar = tk.Scrollbar(
-    load_quiz_frame,
-    orient="vertical",
-    command=canvas_container.yview
-    )
-canvas_container.create_window(
-    (0, 0),
-    window=options,
-    anchor="nw"
-    )
-# Getting quiz titles and creating buttons for list
 quiz_titles = get_quiz()
-for item in quiz_titles:
-    quiz_no = quiz_titles.index(item)
-    button = tk.Button(
-        master=options,
-        text=item,
-        wraplength=1394,
-        justify="left",
-        font=("Helvetica", 20),
-        bg="lightblue",
-        activebackground="#5391b0",
-        padx=50,
-        width=87,
-        # command=lambda: show_frame(quiz_frame)
+# Checks if there are quizzes to load
+if len(quiz_titles) == 0:
+    error_label = tk.Label(
+        master=load_quiz_frame,
+        text="Unable to load quizzes.\nNo quizzes found.",
+        font=("Helvetica", 32),
+        bg="white"
         )
-    button.grid(row=quiz_no, column=0, sticky="ew")
-# Pack everything
-options.update()
-canvas_container.configure(
-    yscrollcommand=scrollbar.set,
-    scrollregion="0 0 0 %s" % options.winfo_height()
+    back_button = tk.Button(
+        master=load_quiz_frame,
+        text="Back to title screen",
+        width=18,
+        bd=0, bg="lightblue",
+        activebackground="#5391b0",
+        font=("Helvetica", 40),
+        command=lambda: show_frame(title_frame)
     )
+    # Pack everything
+    error_label.grid(row=0, column=0, sticky="ew", padx=490, pady=100)
+    back_button.grid(row=1, column=0, sticky="ew", padx=490, pady=100)
+else:
+    # Heading text
+    heading = tk.Label(
+        master=load_quiz_frame,
+        text="Choose a quiz!",
+        font=("Helvetica", 32),
+        bg="white"
+        )
+    heading.grid(row=0, column=0, padx=150, pady=100)
+    # Quiz Selector
+    load_quiz_frame.grid_columnconfigure(0, weight=1)
+    canvas_container = tk.Canvas(load_quiz_frame, height=480)
+    options = tk.Frame(canvas_container)
+    scrollbar = tk.Scrollbar(
+        load_quiz_frame,
+        orient="vertical",
+        command=canvas_container.yview
+        )
+    canvas_container.create_window(
+        (0, 0),
+        window=options,
+        anchor="nw"
+        )
+    # Getting quiz titles and creating buttons for list
+    for item in quiz_titles:
+        quiz_no = quiz_titles.index(item)
+        button = tk.Button(
+            master=options,
+            text=item,
+            wraplength=1394,
+            justify="left",
+            font=("Helvetica", 20),
+            bg="lightblue",
+            activebackground="#5391b0",
+            padx=50,
+            width=87,
+            # command=lambda: show_frame(quiz_frame)
+            )
+        button.grid(row=quiz_no, column=0, sticky="ew")
+    # Pack everything
+    options.update()
+    canvas_container.configure(
+        yscrollcommand=scrollbar.set,
+        scrollregion="0 0 0 %s" % options.winfo_height()
+        )
 
-canvas_container.grid(row=1, column=0, sticky="ew")
-scrollbar.grid(row=1, column=1, sticky="ns")
-load_quiz_frame.grid(row=0, column=0)
+    canvas_container.grid(row=1, column=0, sticky="ew")
+    scrollbar.grid(row=1, column=1, sticky="ns")
+    load_quiz_frame.grid(row=0, column=0)
 
 # Run mainloop
 if __name__ == "__main__":
